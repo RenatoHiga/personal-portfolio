40cbbffee944cf59f344bdfdd414e128fa90e327
renato <renatoka14@gmail.com>
2022-05-27 19:32:06 -0300

    Item component for carousel and next item animation working

--- my-portfolio/src/components/carousel/carousel.js
+++ my-portfolio/src/components/carousel/carousel.js
@@ -1,67 +1,106 @@
 import { Component, h } from "preact";
 
+import Item from "./item";
+
 class Carousel extends Component {
   constructor() {
     super();
 
+    // Create separate array with itemsData and another array, but empty, 
+    // this empty one will be filled with html data
     this.state = {
       activeItemIndex: 0,
-      itemsHtml: [
-        <div class="personal-projects__image-container active">
-          <div class="personal-projects__image-overlay">
-            <h1 class="text-center huge-title">Fitness Training - TCC</h1>
-            <h2 class="text-center mb-50 personal-projects__small-description">
-              Trabalho de Conclusão de Curso: Sistema de Acadêmia para Alunos e
-              Professores
-            </h2>
-          </div>
-          <img
-            src="assets/images/personal-projects/fitness-training/cadastra_exercicio.png"
-            class="personal-projects__image"
-          />
-        </div>,
-        <div class="personal-projects__image-container d-none">
-          <div class="personal-projects__image-overlay">
-            <h1 class="text-center huge-title">Portifólio Pessoal</h1>
-            <h2 class="text-center mb-50 personal-projects__small-description">
-              Portifólio pessoal com design simples para exibir minhas
-              habilidades e projetos
-            </h2>
-          </div>
-          <img
-            src="images/personal-projects/personal-portifolio/personal-portifolio-main.png"
-            class="personal-projects__image"
-          />
-        </div>,
-        <div class="personal-projects__image-container d-none">
-          <div class="personal-projects__image-overlay">
-            <h1 class="text-center huge-title">Portifólio Pessoal</h1>
-            <h2 class="text-center mb-50 personal-projects__small-description">
-              Portifólio pessoal com design simples para exibir minhas
-              habilidades e projetos
-            </h2>
-          </div>
-          <img
-            src="images/personal-projects/placeholder/placeholder.jpg"
-            class="personal-projects__image"
-          />
-        </div>,
+      items: [
+        {
+          title: "Fitness Training - TCC",
+          description: "Trabalho de Conclusão de Curso: Sistema de Acadêmia para Alunos e Professores",
+          imgName: "cadastra_exercicio.png",
+          imgFolder: "fitness-training",
+          active: true,
+          activateNextImageAnimation: false,
+          activatePreviousImageAnimation: false,
+        },
+        {
+          title: "Portifólio Pessoal",
+          description: "Portifólio pessoal com design simples para exibir minhas habilidades e projetos",
+          imgName: "personal-portfolio-main.png",
+          imgFolder: "personal-portfolio",
+          active: false,
+          activateNextImageAnimation: false,
+          activatePreviousImageAnimation: false,
+        },
+        {
+          title: "Portifólio Pessoal",
+          description: "Portifólio pessoal com design simples para exibir minhas habilidades e projetos",
+          imgName: "placeholder.jpg",
+          imgFolder: "placeholder",
+          active: false,
+          activateNextImageAnimation: false,
+          activatePreviousImageAnimation: false,
+        }
       ],
+      itemsHtml: [],
     };
+
+    this.renderItems();
+
+  }
+
+  renderItems = () => {
+    let itemsHtml = this.state.items.map(item => (
+      <Item 
+        title={item.title} 
+        description={item.description} 
+        imgName={item.imgName}
+        imgFolder={item.imgFolder}
+        active={item.active}
+        activateNextImageAnimation={item.activateNextImageAnimation}
+        activatePreviousImageAnimation={item.activatePreviousImageAnimation}
+      />
+    ));
+    this.setState({itemsHtml: itemsHtml});
   }
 
   nextItem = () => {
+    const previousIndex = this.state.activeItemIndex;
+
     const finalIndex = this.state.itemsHtml.length - 1;
     const isLastIndex = finalIndex === this.state.activeItemIndex;
-
-    console.log(finalIndex, this.state.activeItemIndex);
 
     if (isLastIndex) {
       this.state.activeItemIndex = 0;
     } else {
       this.state.activeItemIndex = this.state.activeItemIndex + 1;
     }
+
+    let items = this.state.items;
+    const activeItem   = items[this.state.activeItemIndex];
+    const previousItem = items[previousIndex];
+
+    activeItem.active = true;
+    activeItem.activateNextImageAnimation = true;
+
+    previousItem.active = false;
+    previousItem.activateNextImageAnimation     = false;
+    previousItem.activatePreviousImageAnimation = false;
+
+    this.renderItems();
   };
+
+  // TODO: Develop the logic for the previous item animation!
+  previousItem = () => {
+
+    const firstIndex = 0;
+    const isFirstIndex = firstIndex === this.state.activeItemIndex;
+
+    if (isFirstIndex) {
+      const lastIndex = this.state.itemsHtml.length - 1;
+      this.state.activeItemIndex = lastIndex;
+    } else {
+      this.state.activeItemIndex = this.state.activeItemIndex - 1;
+    }
+
+  }
 
   render() {
     return (
@@ -70,16 +109,16 @@ class Carousel extends Component {
 
         {/* <!-- Buttons --> */}
         <div class="personal-projects__button-container personal-projects__button-container--left">
-          <button class="personal-projects__button" onclick={this.nextItem}>
+          <button class="personal-projects__button" onclick={this.previousItem}>
             <img src="images/icons/arrow_back.svg" alt="Projeto anterior" />
           </button>
         </div>
 
-        {/* <div class="personal-projects__button-container personal-projects__button-container--right">
-          <button class="personal-projects__button" onclick={nextImage}>
+        <div class="personal-projects__button-container personal-projects__button-container--right">
+          <button class="personal-projects__button" onclick={this.nextItem}>
             <img src="images/icons/arrow_forward.svg" alt="Próximo projeto" />
           </button>
-        </div> */}
+        </div>
       </div>
     );
   }
---
+++ my-portfolio/src/components/carousel/item.js
@@ -0,0 +1,46 @@
+import { Component, h } from "preact";
+
+class Item extends Component {
+    constructor(props) {
+        super(props);
+        console.log('rerender');
+
+    }
+
+    addClasses = () => {
+        console.log(this.props.active);
+        if (this.props.active) {
+            
+            if (this.props.activateNextImageAnimation) {
+                return "personal-projects__image-container active personal-projects__next-image"
+            } else if (this.props.activatePreviousImageAnimation) {
+                return "personal-projects__image-container active personal-projects__previous-image"
+            } else {
+                return "personal-projects__image-container active"
+            }
+
+        } else {
+            return "personal-projects__image-container d-none"
+        }
+    }
+
+    render() {
+        return (
+            <div class={this.addClasses()}>
+                <div class="personal-projects__image-overlay">
+                    <h1 class="text-center huge-title">{this.props.title}</h1>
+                    <h2 class="text-center mb-50 personal-projects__small-description">
+                        {this.props.description}
+                    </h2>
+                </div>
+                <img
+                    src={'assets/images/personal-projects/' + this.props.imgFolder + '/' + this.props.imgName}
+                    class="personal-projects__image"
+                />
+            </div>
+        );
+    }
+
+}
+
+export default Item;
